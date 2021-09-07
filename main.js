e54f8d285b53be0f0b388e3b936bbc84a48d9da3
InventorBoy-APP <78768396+InventorBoy-APP@users.noreply.github.com>
2021-09-06 20:07:20 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ menu_list_array = ["Veg Margherita Pizza","Pepperoni Pizza","Cheese Pizza","Tand
 
 function getmenu(){
 var htmldata;
-htmldata="<ol class="menulist;">"
+htmldata="<ol class='menulist'>"
 menu_list_array.sort();
-for(var i=0;i<menu_list_array.length,i,++){
+for(var i=0;i<menu_list_array.length;i++){
     htmldata=htmldata+'<li>'+menu_list_array[i]+'</li>'
 }
 html=htmldata+"</ol>"
@@ -14,10 +14,10 @@ document.getElementById("display_menu").innerHTML=htmldata;
 function add_item(){
 var htmldata;
 var item=document.getElementById("add_item").value;
-//Complete the code
-
+menu_list_array.push(item);
+menu_list_array.sort(item);
 }
 
 function add_top(){
-//Complete the code
+   
 }
